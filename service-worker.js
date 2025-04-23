1b9e7aed84bcb882e782921358dc743d16e64265
Julio Camacho C <p92camcj@gmail.com>
2025-04-23 18:34:55 +0200

    Nuevo orden botón jugar y service-worker nuevos archivos
    En el index se añade nuevo orden para que los botones jugar y cancelar se pueda "mover de sitio".
    En el service-worker se añaden los archivos JS que aún no estaban incluidos

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,17 @@
-const CACHE_NAME = 'invocadores-v1';
+const CACHE_NAME = 'invocadores-v1.1.0';
 const urlsToCache = [
   './',
   './index.html',
   './style.css',
+  './version.json',
   './js/index.js',
   './js/utils.js',
   './js/setup.js',
   './js/game.js',
   './js/abilities.js',
   './js/actions.js',
-  './js/render.js'
+  './js/render.js',
+  './js/version-check.js'
 ];
 
 self.addEventListener('install', event => {
